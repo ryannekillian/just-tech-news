01a3f2dc9ed0aaffc716e5d3f158f6ecd7b40287
ryannekillian <ryanne.killian@gmail.com>
2020-08-12 23:01:48 -0500

    update password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,33 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+        
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+          if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        //res.json({ user: dbUserData });
+        // add comment syntax in front of this line in the .then()
+        // res.json({ user: dbUserData }
+    
+      });  
+    });
   
 
 // PUT /api/users/1
